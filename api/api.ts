c8917ed7867d382f790046e4163862f8af75d946
cp985 <166384434+cp985@users.noreply.github.com>
2026-08-08 14:19:22 +0200

    no changes1

--- api/api.ts
+++ api/api.ts
@@ -17,19 +17,7 @@ try{
 }
 }
 
-//limited dossiers list
 
-// export const getLimitDossiers  = async () : Promise<DbDossier[]> =>  {
-// try{
-//     const response = await fetch(process.env.NEXT_PUBLIC_URL_RENDER + "/dossiers?limit=3");
-//     const data = await response.json();
-//     console.log(data);
-//     return data as DbDossier[];
-// } catch (error) {
-//     console.log(error);
-//     return [];
-// }
-// }
 
 
 export const getDossierByCode  = async (code:string) : Promise<DbDossier[]> =>  {
